32a74b86149a495bece1619ae749c11d1fe0c646
Fepozopo <fepozopo.f39@gmail.com>
2025-01-14 19:17:09 -0800

    Made some progress on re-writing the main function from Python to Go

--- go.mod
+++ go.mod
@@ -3,9 +3,11 @@ module github.com/Fepozopo/bsc-hotsheet-update
 go 1.23.4
 
 require (
+	github.com/TheTitanrain/w32 v0.0.0-20180517000239-4f5cfb03fabf // indirect
 	github.com/mohae/deepcopy v0.0.0-20170929034955-c48cc78d4826 // indirect
 	github.com/richardlehane/mscfb v1.0.4 // indirect
 	github.com/richardlehane/msoleps v1.0.4 // indirect
+	github.com/sqweek/dialog v0.0.0-20240226140203-065105509627 // indirect
 	github.com/xuri/efp v0.0.0-20240408161823-9ad904a10d6d // indirect
 	github.com/xuri/excelize/v2 v2.9.0 // indirect
 	github.com/xuri/nfp v0.0.0-20240318013403-ab9948c2c4a7 // indirect
--- main.go
+++ main.go
@@ -2,8 +2,81 @@ package main
 
 import (
 	"fmt"
+	"log"
+	"time"
+
+	"github.com/sqweek/dialog"
 )
 
 func main() {
-	fmt.Println("Work in progress...")
+	startTime := time.Now()
+
+	for {
+		var hotsheet string
+		fmt.Print("Which hotsheet do you want to update? (smd, bsc, 21c, exit): ")
+		fmt.Scanln(&hotsheet)
+
+		switch hotsheet {
+		case "smd":
+			fileSMDHotsheet, err := dialog.File().Title("Select the SMD HOTSHEET...").Filter("Excel Files", "*.xlsx").Load()
+			if err != nil {
+				log.Fatal(err)
+			}
+			fileSMDStockReport, err := dialog.File().Title("Select the SMD Stock Report...").Filter("Excel Files", "*.xlsx").Load()
+			if err != nil {
+				log.Fatal(err)
+			}
+			fileSMDSalesReport, err := dialog.File().Title("Select the SMD Sales Report...").Filter("Excel Files", "*.xlsx").Load()
+			if err != nil {
+				log.Fatal(err)
+			}
+
+			// HOTSHEET | SECTION | REPORT | SKU | ON HAND | ON PO | ON SO/BO
+			smdStock := UpdateStock{fileSMDHotsheet, "EVERYDAY", fileSMDStockReport, 5, 6, 9, 11}
+			smdStockHoliday := UpdateStock{fileSMDHotsheet, "HOLIDAY", fileSMDStockReport, 3, 4, 6, 8}
+			// HOTSHEET | SECTION | REPORT | SKU | YTD
+			smdSales := UpdateSales{fileSMDHotsheet, "EVERYDAY", fileSMDSalesReport, 5, 16}
+			smdSalesHoliday := UpdateSales{fileSMDHotsheet, "HOLIDAY", fileSMDSalesReport, 3, 14}
+
+			// Get user input for which sections to update
+			for {
+				var section string
+				fmt.Print("Which section do you want to update? (everyday, holiday, all, exit): ")
+				fmt.Scanln(&section)
+
+				switch section {
+				case "everyday":
+					smdStock.handlerUpdateStock()
+					smdSales.handlerUpdateSales()
+				case "holiday":
+					smdStockHoliday.handlerUpdateStock()
+					smdSalesHoliday.handlerUpdateSales()
+				case "all":
+					smdStock.handlerUpdateStock()
+					smdStockHoliday.handlerUpdateStock()
+					smdSales.handlerUpdateSales()
+					smdSalesHoliday.handlerUpdateSales()
+				case "exit":
+				default:
+					fmt.Println("Invalid input. Please enter 'everyday', 'holiday', 'all', or 'exit'.")
+				}
+			}
+
+		case "bsc":
+			// Similar logic for BSC hotsheet
+			// TODO Implement file selection and updates for BSC here
+
+		case "21c":
+			// Similar logic for 21c hotsheet
+			// TODO Implement file selection and updates for 21c here
+
+		case "exit":
+			return
+		default:
+			fmt.Println("Invalid input. Please enter 'smd', 'bsc', '21c', or 'exit'.")
+		}
+
+		timeElapsed := time.Since(startTime)
+		fmt.Printf("Done!\nElapsed time: %s\n", timeElapsed)
+	}
 }
