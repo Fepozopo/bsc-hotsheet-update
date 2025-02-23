0cc40d6a6e598836e5057688437fa150bfccddc8
Shane DuPrey <36708583+Fepozopo@users.noreply.github.com>
2025-02-22 16:16:29 -0800

    Updated case_21c.go to update the new version of that hotsheet where holiday product is separate from everyday

--- hotsheet/case_21c.go
+++ hotsheet/case_21c.go
@@ -20,6 +20,26 @@ func Case21C(fileHotsheetNew, fileReport string) error {
 		OnSOBOCol:        "G",
 		YtdSoldIssuedCol: "M",
 	}
+	winter := Update{
+		Hotsheet:         fileHotsheetNew,
+		Sheet:            "Winter Holiday",
+		Report:           fileReport,
+		SkuCol:           "C",
+		OnHandCol:        "D",
+		OnPOCol:          "E",
+		OnSOBOCol:        "G",
+		YtdSoldIssuedCol: "M",
+	}
+	spring := Update{
+		Hotsheet:         fileHotsheetNew,
+		Sheet:            "Spring Holiday",
+		Report:           fileReport,
+		SkuCol:           "C",
+		OnHandCol:        "D",
+		OnPOCol:          "E",
+		OnSOBOCol:        "G",
+		YtdSoldIssuedCol: "M",
+	}
 
 	// Update the hotsheet
 	err := everyday.Update("21c", "everyday")
@@ -27,6 +47,18 @@ func Case21C(fileHotsheetNew, fileReport string) error {
 		return fmt.Errorf("failed to update stock: %w", err)
 	}
 	fmt.Println("Everyday product updated successfully")
+
+	err = winter.Update("21c", "winter")
+	if err != nil {
+		return fmt.Errorf("failed to update stock: %w", err)
+	}
+	fmt.Println("Winter product updated successfully")
+
+	err = spring.Update("21c", "spring")
+	if err != nil {
+		return fmt.Errorf("failed to update stock: %w", err)
+	}
+	fmt.Println("Spring product updated successfully")
 
 	return nil
 }
